5ac76ac7c9fe3263f7f6aa7bc62cc11ab85eb6db
Bunji75 <daynelinke@gmail.com>
2025-05-19 10:53:02 +1000

    Started adding new pages

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          This is the About page
+        </h1>
+
+        <p>
+          Edit me and fill me with projects.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+
+function Projects() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          This is the projects page
+        </h1>
+
+        <p>
+          Edit me and fill me with projects.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Projects;
